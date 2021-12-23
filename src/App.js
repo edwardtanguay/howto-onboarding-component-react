34342009f8e59bd60f4842985893da33b3db1905
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-12-23 14:15:35 +0100

    finished

--- src/App.js
+++ src/App.js
@@ -7,17 +7,20 @@ const StepOne = ({ gotoNext }) => {
 	return (
 		<>
 			<p>Name: <input type="text" ref={field_name} /></p>
-			<button onClick={() => gotoNext({ name: field_name.current.value})}>Next</button>
+			<button onClick={() => gotoNext({ name: field_name.current.value })}>Next</button>
 		</>
 	)
 };
 
-const StepTwo = ({ gotoNext }) => (
-	<>
-		<p>Age: <input type="text" /></p>
-		<button onClick={() => gotoNext({ age: 81 })}>Next</button>
-	</>
-);
+const StepTwo = ({ gotoNext }) => {
+	const field_age = React.createRef();
+	return (
+		<>
+			<p>Age: <input type="text" ref={field_age} /></p>
+			<button onClick={() => gotoNext({ age: field_age.current.value })}>Next</button>
+		</>
+	)
+};
 
 const StepThree = ({ gotoNext }) => (
 	<>
@@ -26,46 +29,58 @@ const StepThree = ({ gotoNext }) => (
 	</>
 );
 
-const StepFour = ({ gotoNext }) => (
-	<>
-		<p>City: <input type="text" /></p>
-		<button onClick={() => gotoNext({ city: "Berlin" })}>Next</button>
-	</>
-);
+const StepFour = ({ gotoNext }) => {
+	const field_city = React.createRef();
+	return (
+		<>
+			<p>City: <input type="text" ref={field_city} /></p>
+			<button onClick={() => gotoNext({ city: field_city.current.value })}>Next</button>
+		</>
+	)
+};
 
 function App() {
 	const [onboardingData, setOnboardingData] = useState({});
 	const [currentIndex, setCurrentIndex] = useState(0);
 	const [message, setMessage] = useState('');
+	const [finished, setFinished] = useState(false);
 
 
 	useEffect(() => {
-		const onFinish = () => setMessage(`finished, send this to backend: ${JSON.stringify(onboardingData)}`);
+		if (finished) setMessage(`finished, send this to backend: ${JSON.stringify(onboardingData, null, 2)}`);
+	}, [finished, onboardingData])
 
-		if (currentIndex > 3) {
-			onFinish();
-		}
-	}, [currentIndex, onboardingData])
+	const onNext = (stepData, isLastStep = false) => {
 
-	const onNext = (stepData, onFinish) => {
 		setOnboardingData({ ...onboardingData, ...stepData });
 		setCurrentIndex(currentIndex + 1);
+		if (isLastStep) setFinished(true);
 	}
 
 	return (
 		<div className="App">
 			{message !== '' && (
-				<div className="message">{message}</div>
+				<pre className="message">{message}</pre>
 			)}
-			<OnboardingFlow
-				currentIndex={currentIndex}
-				onNext={onNext}
-			>
-				<StepOne />
-				<StepTwo />
-				<StepThree />
-				<StepFour />
-			</OnboardingFlow>
+			{message === '' && (
+				<div className="row">
+					<div className="col s12 m6">
+						<div className="card blue-grey darken-3">
+							<div className="card-content white-text">
+								<OnboardingFlow
+									currentIndex={currentIndex}
+									onNext={onNext}
+								>
+									<StepOne />
+									<StepTwo />
+									{Number(onboardingData.age) >= 60 && <StepThree />}
+									<StepFour isLastStep={true} />
+								</OnboardingFlow>
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
--- src/components/OnboardingFlow.js
+++ src/components/OnboardingFlow.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export const OnboardingFlow = ({ children, onFinish, currentIndex, onNext }) => {
-	const gotoNext = stepData => {
-		onNext(stepData, onFinish);
-	}
-
+export const OnboardingFlow = ({ children, currentIndex, onNext }) => {
 	const currentChild = React.Children.toArray(children)[currentIndex];
+
+	const gotoNext = stepData => {
+		onNext(stepData, currentChild.props.isLastStep);
+	}
 
 	if (React.isValidElement(currentChild)) {
 		return React.cloneElement(currentChild, { gotoNext });
